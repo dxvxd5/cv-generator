8a3c490d4b72cb98cd362cb7123ff3f41b375da5
David Kragbé <42718923+dxvxd5@users.noreply.github.com>
2022-07-28 20:34:19 +0200

    Create Prometheus project converter Fixes #2

--- src/converters/prometheus/prometheus.py
+++ src/converters/prometheus/prometheus.py
@@ -1,6 +1,9 @@
 from sections.education import Education
+from sections.project import Project
 from sections.skill import Skill
 from utils.latex import Latex
+
+from .utils import to_location, to_period, to_school_with_location
 
 
 class PrometheusConverter:
@@ -19,9 +22,9 @@ class PrometheusConverter:
         """
         Convert the education object to Latex
         """
-        period = f"{education.start_date} - {education.end_date}"
-        location = f"{education.city}, {education.country}"
-        school = f"{education.school} - {location}"
+        period = to_period(education.start_date, education.end_date)
+        location = to_location(education.city, education.country)
+        school = to_school_with_location(education.school, location)
         description = Latex.build_itemize_block(education.description)
         degree = Latex.bold(education.degree)
 
@@ -40,3 +43,18 @@ class PrometheusConverter:
         Convert the skill object to Latex
         """
         return f"{Latex.bold(skill.area + ': ' )}{Latex.to_dot_separated_items(skill.skills)}"
+
+    def convert_project(project: Project):
+        """
+        Convert the project object to Latex
+        """
+        description = Latex.build_itemize_block(project.description)
+        period = to_period(project.start_date, project.end_date)
+        location = to_location(project.city, project.country)
+        title = (
+            project.link and Latex.link(project.link, project.title) or project.title
+        )
+
+        return PrometheusConverter.build_datedsubsection_cmd(
+            [period, location, project.context, title, description]
+        )
