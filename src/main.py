9324ffe6c170678e8c141096a5606f1477d5f640
David Kragbé <42718923+dxvxd5@users.noreply.github.com>
2022-08-20 22:35:41 +0200

    Make the compilation to latex happen in the main script Fixes #35

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 import json
+import os
+import tempfile
+from distutils import dir_util, file_util
 from io import TextIOWrapper
 
 import click
 
 from converters.prometheus.prometheus import PrometheusConverter
 from sections.cv import CV
+from utils.latex import Latex
 from utils.path import change_extension
 
 
@@ -41,9 +45,24 @@ def main(input: TextIOWrapper, output: str, template: str):
 
     converter = get_converter(template)
     cv = CV(**json.load(input))
-
     output_file = output or change_extension(input.name, ".pdf")
-    converter.convert_cv_to_pdf(cv, output_file)
+
+    tmpdir_path = tempfile.mkdtemp()
+    prev_umask = os.umask(0o077)
+
+    try:
+        main_latex_file = converter.generate_latex_files(cv, tmpdir_path)
+        pdf_file = Latex.compile_file(main_latex_file)
+
+    except IOError as e:
+        raise e
+
+    else:
+        file_util.copy_file(pdf_file, output_file)
+
+    finally:
+        os.umask(prev_umask)
+        dir_util.remove_tree(tmpdir_path)
 
 
 if __name__ == "__main__":
